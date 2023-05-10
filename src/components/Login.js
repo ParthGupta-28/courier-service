857d92c6a689037493c198be4034ae469938a520
ParthGupta-28 <pg6272695@gmail.com>
2023-05-11 01:57:41 +0530

    com

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ export default function Login({
   async function onClickLogin() {
     try {
       const res = await axios.get(
-        `https://courier-backend-vvsg.onrender.com/users/${creds["email"]}/${creds["password"]}`
+        `http://localhost:8080/users/${creds["email"]}/${creds["password"]}`
       );
 
       setUserDetails((prevDetails) => {
